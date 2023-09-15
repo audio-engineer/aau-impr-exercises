c00c5d8c61882bb3bd3e51a94d78e26cdc61bf1d
Martin Kedmenec <martin.kedmenec@outlook.com>
2023-09-15 09:47:56 +0200

    Update exercise 2_5

--- 2_5/src/main.c
+++ 2_5/src/main.c
@@ -3,60 +3,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool IsScanfInputValid(int number_of_assigned_input_items) {
-  if (number_of_assigned_input_items == EOF ||
-      number_of_assigned_input_items != 1) {
-    printf("Invalid number_of_assigned_input_items. Try again.\n");
-
-    while (fgetc(stdin) != '\n') {
-    }
-
-    return false;
-  }
-
-  return true;
-}
-
 int main() {
-  char* input_seconds_string = malloc(sizeof(char));
-  unsigned long input_seconds_long = 0;
+  char input_buffer[kBufferSize];
   char* end = NULL;
 
   printf("Enter a number of seconds: ");
 
-  int number_of_assigned_input_items = scanf("%s", input_seconds_string);
+  char* output = fgets(input_buffer, (int)sizeof(input_buffer), stdin);
 
-  if (!IsScanfInputValid(number_of_assigned_input_items)) {
-    free(input_seconds_string);
-
+  if (output == NULL) {
     return EXIT_FAILURE;
   }
 
-  input_seconds_long = strtol(input_seconds_string, &end, kBase);
-  free(input_seconds_string);
+  unsigned long input_seconds_long = strtol(input_buffer, &end, kBase);
+
+  if (*end != '\n') {
+    return EXIT_FAILURE;
+  }
 
   unsigned long seconds_per_minute = kSeconds;
   unsigned long seconds_per_hour = seconds_per_minute * kSeconds;
   unsigned long seconds_per_day = seconds_per_hour * kHours;
   unsigned long seconds_per_week = seconds_per_day * kDays;
 
-  unsigned long weeks = 0;
-  unsigned long days = 0;
-  unsigned long hours = 0;
-  unsigned long minutes = 0;
-  unsigned long seconds = 0;
-
-  weeks = input_seconds_long / seconds_per_week;
+  unsigned long weeks = input_seconds_long / seconds_per_week;
   input_seconds_long %= seconds_per_week;
 
-  days = input_seconds_long / seconds_per_day;
+  unsigned long days = input_seconds_long / seconds_per_day;
   input_seconds_long %= seconds_per_day;
 
-  hours = input_seconds_long / seconds_per_hour;
+  unsigned long hours = input_seconds_long / seconds_per_hour;
   input_seconds_long %= seconds_per_hour;
 
-  minutes = input_seconds_long / seconds_per_minute;
-  seconds = input_seconds_long % seconds_per_minute;
+  unsigned long minutes = input_seconds_long / seconds_per_minute;
+
+  unsigned long seconds = input_seconds_long % seconds_per_minute;
 
   printf("This number of seconds is equivalent to:\n");
   printf("Weeks: %ld\n", weeks);
--- 2_5/src/main.h
+++ 2_5/src/main.h
@@ -1,11 +1,10 @@
 #ifndef EXERCISES_2_5_SRC_MAIN_H_
 #define EXERCISES_2_5_SRC_MAIN_H_
 
+const unsigned int kBufferSize = 30;
 const int kBase = 10;
 const unsigned long kSeconds = 60;
 const unsigned long kHours = 24;
 const unsigned long kDays = 7;
-
-bool IsScanfInputValid(int number_of_assigned_input_items);
 
 #endif  // EXERCISES_2_5_SRC_MAIN_H_
