f73d85e220280aaba763f333f58dadce4e0a56fe
Martin Kedmenec <martin.kedmenec@outlook.com>
2023-11-25 22:13:19 +0100

    Finish exam assignment 2

--- e_2/src/main.c
+++ e_2/src/main.c
@@ -2,19 +2,50 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-U16 GetDiceRoll() {
+char* ReadUserInput(const char* const message) {
+  printf("%s ", message);
+
+  char* input = calloc(kBufferSize, sizeof(char));
+
+  if (!input) {
+    perror("Memory allocation failed");
+
+    return NULL;
+  }
+
+  if (!fgets(input, kBufferSize, stdin)) {
+    free(input);
+
+    return NULL;
+  }
+
+  const size_t kUserInputLength = strlen(input);
+
+  if (1 > kUserInputLength || '\n' != input[kUserInputLength - 1]) {
+    free(input);
+
+    return NULL;
+  }
+
+  input[kUserInputLength - 1] = '\0';
+
+  return input;
+}
+
+int GetDiceRoll() {
   // NOLINTNEXTLINE(concurrency-mt-unsafe)
   return (rand() % (kDiceUpperBound - kDiceLowerBound + 1)) + kDiceLowerBound;
 }
 
-U16 GetOccurrenceCountOfTargetOutcome(const U16 target_outcome,
-                                      const U16* dice_roll_results_buffer,
-                                      const U16 number_of_dice) {
-  U16 occurrence_count = 0;
+int GetOccurrenceCountOfTargetOutcome(const int target_outcome,
+                                      const int* dice_roll_results_buffer,
+                                      const int number_of_dice) {
+  int occurrence_count = 0;
 
-  for (U16 dice = 0; dice < number_of_dice; dice++) {
+  for (int dice = 0; dice < number_of_dice; dice++) {
     if (target_outcome != dice_roll_results_buffer[dice]) {
       continue;
     }
@@ -25,23 +56,99 @@ U16 GetOccurrenceCountOfTargetOutcome(const U16 target_outcome,
   return occurrence_count;
 }
 
-void RunRound(const U16 number_of_dice, U16* dice_roll_results_buffer) {
-  for (U16 dice = 0; dice < number_of_dice; dice++) {
+void RunRound(const int number_of_dice, int* dice_roll_results_buffer) {
+  for (int dice = 0; dice < number_of_dice; dice++) {
     dice_roll_results_buffer[dice] = GetDiceRoll();
   }
 }
 
-U16 CheckForFourOfAKind(const U16 number_of_dice,
-                        U16* dice_roll_results_buffer) {
+void CheckForOnePair(const int number_of_dice, int* dice_roll_results_buffer) {
+  RunRound(number_of_dice, dice_roll_results_buffer);
+
+  printf("One pair:\t\t\t");
+
+  for (int dice = 0; dice < number_of_dice; dice++) {
+    printf("%d ", dice_roll_results_buffer[dice]);
+  }
+
+  int pair_dice_value = 0;
+  int number_of_matches = 0;
+
+  for (int die_value = kDiceUpperBound; die_value > kDiceLowerBound - 1;
+       die_value--) {
+    if (2 == number_of_matches) {
+      break;
+    }
+
+    number_of_matches = 0;
+
+    for (int dice = 0; dice < number_of_dice; dice++) {
+      if (die_value != dice_roll_results_buffer[dice]) {
+        continue;
+      }
+
+      if (2 > number_of_matches) {
+        pair_dice_value = die_value;
+        number_of_matches++;
+
+        if (2 == number_of_matches) {
+          break;
+        }
+
+        continue;
+      }
+
+      break;
+    }
+  }
+
+  printf("-- Sum of the largest pair: %d", pair_dice_value * 2);
+  printf("\n");
+}
+
+void CheckForTwoPairs(const int number_of_dice, int* dice_roll_results_buffer) {
+  RunRound(number_of_dice, dice_roll_results_buffer);
+
+  printf("Two pairs:\t\t\t");
+
+  for (int dice = 0; dice < number_of_dice; dice++) {
+    printf("%d ", dice_roll_results_buffer[dice]);
+  }
+
+  int pair_dice_value = 0;
+  bool two_pairs_found = false;
+
+  for (int die_value = kDiceUpperBound; die_value > kDiceLowerBound - 1;
+       die_value--) {
+    if (two_pairs_found) {
+      break;
+    }
+
+    int count = GetOccurrenceCountOfTargetOutcome(
+        die_value, dice_roll_results_buffer, number_of_dice);
+
+    if (4 <= count) {
+      two_pairs_found = true;
+
+      pair_dice_value = die_value;
+    }
+  }
+
+  printf("-- Sum of the two largest pairs: %d", pair_dice_value * 4);
+  printf("\n");
+}
+
+int CheckForFourOfAKind(const int number_of_dice,
+                        int* dice_roll_results_buffer) {
   RunRound(number_of_dice, dice_roll_results_buffer);
 
   printf("Four of a Kind:\t\t");
 
-  U16 sum_of_four_of_a_kind = 0;
+  int sum_of_four_of_a_kind = 0;
 
-  U16 array[kDiceUpperBound] = {0};
+  int array[kDiceUpperBound] = {0};
 
-  for (U16 dice = 0; dice < number_of_dice; dice++) {
+  for (int dice = 0; dice < number_of_dice; dice++) {
     printf("%d ", dice_roll_results_buffer[dice]);
 
     switch (dice_roll_results_buffer[dice]) {
@@ -68,7 +175,7 @@ U16 CheckForFourOfAKind(const U16 number_of_dice,
     }
   }
 
-  for (U16 i = 0; i < (U16)kDiceUpperBound; i++) {
+  for (int i = 0; i < kDiceUpperBound; i++) {
     if (4 <= array[i]) {
       sum_of_four_of_a_kind = sum_of_four_of_a_kind + ((i + 1) * 4);
     }
@@ -80,19 +187,19 @@ U16 CheckForFourOfAKind(const U16 number_of_dice,
   return sum_of_four_of_a_kind;
 }
 
-void CheckForStraight(const U16 number_of_dice, U16* dice_roll_results_buffer,
-                      const U16 lower_bound, const char* name,
-                      const U16 upper_bound) {
+void CheckForStraight(const int number_of_dice, int* dice_roll_results_buffer,
+                      const int lower_bound, const char* name,
+                      const int upper_bound) {
   RunRound(number_of_dice, dice_roll_results_buffer);
   printf("%s:\t\t", name);
 
   bool outcome_found[kDiceUpperBound - kDiceLowerBound] = {false};
-  U16 extra_points = 0;
+  int extra_points = 0;
 
-  for (U16 dice = 0; dice < number_of_dice; dice++) {
+  for (int dice = 0; dice < number_of_dice; dice++) {
     printf("%d ", dice_roll_results_buffer[dice]);
 
-    for (U16 outcome = lower_bound; outcome < upper_bound + 1; outcome++) {
+    for (int outcome = lower_bound; outcome < upper_bound + 1; outcome++) {
       if (outcome == dice_roll_results_buffer[dice] &&
           !outcome_found[outcome - lower_bound]) {
         outcome_found[outcome - lower_bound] = true;
@@ -102,9 +209,8 @@ void CheckForStraight(const U16 number_of_dice, U16* dice_roll_results_buffer,
     }
   }
 
-  for (U16 outcome_index = 0;
-       outcome_index < (U16)(kDiceUpperBound - kDiceLowerBound);
-       outcome_index++) {
+  for (int outcome_index = 0;
+       outcome_index < (kDiceUpperBound - kDiceLowerBound); outcome_index++) {
     if (!outcome_found[outcome_index]) {
       extra_points = 0;
 
@@ -116,22 +222,103 @@ void CheckForStraight(const U16 number_of_dice, U16* dice_roll_results_buffer,
   printf("\n");
 }
 
-void RunUpperSection(const U16 number_of_dice, U16* dice_roll_results_buffer) {
+void CheckForFullHouse(const int number_of_dice,
+                       int* dice_roll_results_buffer) {
+  RunRound(number_of_dice, dice_roll_results_buffer);
+
+  printf("Full House:\t\t\t");
+
+  for (int dice = 0; dice < number_of_dice; dice++) {
+    printf("%d ", dice_roll_results_buffer[dice]);
+  }
+
+  int three_of_a_kind = 0;
+  int pair = 0;
+
+  for (int die_value = kDiceUpperBound; die_value >= kDiceLowerBound;
+       die_value--) {
+    int count = GetOccurrenceCountOfTargetOutcome(
+        die_value, dice_roll_results_buffer, number_of_dice);
+
+    if (count >= 3 && three_of_a_kind == 0) {
+      three_of_a_kind = die_value;
+    } else if (count >= 2) {
+      pair = die_value;
+    }
+  }
+
+  if (three_of_a_kind != 0 && pair != 0) {
+    printf("-- Full House with three %d's and two %d's: %d", three_of_a_kind,
+           pair, three_of_a_kind + pair * 2);
+  }
+
+  printf("\n");
+}
+
+void CheckForChance(const int number_of_dice, int* dice_roll_results_buffer) {
+  RunRound(number_of_dice, dice_roll_results_buffer);
+
+  printf("Chance:\t\t\t\t");
+
+  for (int dice = 0; dice < number_of_dice; dice++) {
+    printf("%d ", dice_roll_results_buffer[dice]);
+  }
+
+  int sum = 0;
+
+  for (int dice = 0; dice < number_of_dice; dice++) {
+    sum += dice_roll_results_buffer[dice];
+  }
+
+  printf("-- Sum of all dice: %d", sum);
+  printf("\n");
+}
+
+void CheckForYatzy(const int number_of_dice, int* dice_roll_results_buffer) {
+  RunRound(number_of_dice, dice_roll_results_buffer);
+
+  printf("Yatzy:\t\t\t\t");
+
+  for (int dice = 0; dice < number_of_dice; dice++) {
+    printf("%d ", dice_roll_results_buffer[dice]);
+  }
+
+  int yatzy_value = dice_roll_results_buffer[0];
+  bool is_yatzy = true;
+
+  for (int dice = 1; dice < number_of_dice; dice++) {
+    if (dice_roll_results_buffer[dice] != yatzy_value) {
+      is_yatzy = false;
+
+      break;
+    }
+  }
+
+  if (!is_yatzy) {
+    printf("-- No Yatzy :(");
+  } else {
+    printf("-- Yatzy with all dice showing %d", yatzy_value);
+  }
+
+  printf("\n");
+}
+
+void RunUpperSection(const int number_of_dice, int* dice_roll_results_buffer) {
   printf(
       "UPPER SECTION "
       "------------------------------------------------------------------\n");
 
-  U16 one_indexed_target_outcome = 0;
-  const char* kUpperSectionNames[kUpperSections] = {"Ones",  "Twos",  "Threes",
-                                                    "Fours", "Fives", "Sixes"};
+  int one_indexed_target_outcome = 0;
+  const char* const kUpperSectionNames[kUpperSections] = {
+      "Ones", "Twos", "Threes", "Fours", "Fives", "Sixes"};
 
-  for (U16 target_outcome = 0; target_outcome < (U16)kDiceUpperBound;
+  for (int target_outcome = 0; target_outcome < kDiceUpperBound;
        target_outcome++) {
     RunRound(number_of_dice, dice_roll_results_buffer);
 
     printf("%s:\t\t\t\t", kUpperSectionNames[target_outcome]);
 
-    for (U16 dice = 0; dice < number_of_dice; dice++) {
+    for (int dice = 0; dice < number_of_dice; dice++) {
       printf("%d ", dice_roll_results_buffer[dice]);
     }
 
@@ -146,16 +333,21 @@ void RunUpperSection(const U16 number_of_dice, U16* dice_roll_results_buffer) {
   }
 }
 
-void RunLowerSection(const U16 number_of_dice, U16* dice_roll_results_buffer) {
+void RunLowerSection(const int number_of_dice, int* dice_roll_results_buffer) {
   printf(
       "LOWER SECTION "
       "------------------------------------------------------------------\n");
 
+  CheckForOnePair(number_of_dice, dice_roll_results_buffer);
+  CheckForTwoPairs(number_of_dice, dice_roll_results_buffer);
   CheckForFourOfAKind(number_of_dice, dice_roll_results_buffer);
   CheckForStraight(number_of_dice, dice_roll_results_buffer, kDiceLowerBound,
                    "Small Straight", kDiceUpperBound - 1);
   CheckForStraight(number_of_dice, dice_roll_results_buffer,
                    kDiceLowerBound + 1, "Large Straight", kDiceUpperBound);
+  CheckForFullHouse(number_of_dice, dice_roll_results_buffer);
+  CheckForChance(number_of_dice, dice_roll_results_buffer);
+  CheckForYatzy(number_of_dice, dice_roll_results_buffer);
 }
 
 int main() {
@@ -164,10 +356,15 @@ int main() {
   printf("Welcome to the Yatzy CLI. Copyright © 2023 AAU\n");
   printf("\n");
 
+  char* const kUserInput =
+      ReadUserInput("Enter the number of dice you want to play with:");
+  const int kNumberOfDice = atoi(kUserInput);
+  free(kUserInput);
+
   // NOLINTBEGIN
-  U16* dice_roll_results_buffer = calloc(20, sizeof(U16));
-  RunUpperSection(20, dice_roll_results_buffer);
-  RunLowerSection(20, dice_roll_results_buffer);
+  int* dice_roll_results_buffer = calloc(kNumberOfDice, sizeof(int));
+  RunUpperSection(kNumberOfDice, dice_roll_results_buffer);
+  RunLowerSection(kNumberOfDice, dice_roll_results_buffer);
   free(dice_roll_results_buffer);
   // NOLINTEND
 
--- e_2/src/main.h
+++ e_2/src/main.h
@@ -1,9 +1,12 @@
 #ifndef EXERCISES_E_2_SRC_MAIN_H_
 #define EXERCISES_E_2_SRC_MAIN_H_
 
-typedef unsigned short U16;
-
-enum Constants { kDiceLowerBound = 1, kDiceUpperBound = 6, kUpperSections = 6 };
+enum Constants {
+  kDiceLowerBound = 1,
+  kDiceUpperBound = 6,
+  kUpperSections = 6,
+  kBufferSize = 64
+};
 
 enum Dice { kOne = 1, kTwo, kThree, kFour, kFive, kSix };
 
